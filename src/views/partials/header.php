775368e85c76b3e4b41443c493c810e71ad789ae
Marcelo Silva <marcelo.lp.ops@gmail.com>
2024-04-23 12:12:03 -0300

    primeiro commit

---
+++ src/views/partials/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/imagens/logo.png" type="image/x-icon" />
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="main">
+            <div class="main-top">
+                <div class="mLeft">
+                    <nav>Grupo Nem</nav>
+                </div>
+                <nav class="mRight">
+
+                    <ul>
+                        <li><a href=""> Me Liga</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href=""> Whatsapp</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href=""> Volte a estudar</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href=""> Sou Professor</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href=""> Sou Aluno</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="main-bottom">
+                <a href="">
+                    <img src="<?=$base?>/assets/images/logo.png">
+                </a>
+                <nav></nav>
+                <div></div>
+            </div>
+        </div>
+
+    </header>
