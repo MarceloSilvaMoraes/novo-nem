cf1a640e1348069ccd543629e7aef8c5807727b1
marcelo <marcelo.lp.ops@gmail.com>
2024-04-30 15:24:08 -0300

    conexão com banco de dados e busca nos cursos

--- src/Config.php
+++ src/Config.php
@@ -1,20 +1,35 @@
 <?php
 namespace src;
 
-use PDO;
-
-$pdo = new PDO();
-
 class Config {
     const BASE_DIR = '/novo-nem/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'grup5172_website';
-    CONST DB_USER = 'grup5172_nem';
-    const DB_PASS = 'silva123';
+    const DB_DATABASE = 'test';
+    CONST DB_USER = 'root';
+    const DB_PASS = '';
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
 }
-global $pdo;
+namespace src;
+
+use \PDO;
+class Database {
+    public static function getConnection() {
+        $dsn = Config::DB_DRIVER . ':host=' . Config::DB_HOST . ';dbname=' . Config::DB_DATABASE;
+        $user = Config::DB_USER;
+        $password = Config::DB_PASS;
+
+        try {
+            $connection = new PDO($dsn, $user, $password);
+            // Configura o PDO para lançar exceções em caso de erro
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connection;
+        } catch (\PDOException $e) {
+            // Trate os erros de conexão aqui
+            die('Erro de conexão: ' . $e->getMessage());
+        }
+    }
+}
--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,11 +1,19 @@
 <?php
 namespace src\controllers;
-
+// namespace src\Config;
+// namespace core\Database;
 use \core\Controller;
+use \core\Database;
+// use \src\Config;
 
 class HomeController extends Controller {
 
     public function index() {
+        $connection = Database::getInstance();
+        $statement = $connection->query('SELECT * FROM questoes');
+        $users = $statement->fetchAll();
+        //tabela questoes tem as colunas id, pergunta
+        print_r($users);
         $this->render('home');
     }
 
